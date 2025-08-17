dcc0094f83ae42bea26c157bfd5527393b713ef4
Evandro Bitencourt <contato@flyingweb.com.br>
2025-08-17 12:05:13 -0400

    ajustando a pagina dos questionarios no mobile

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,8 +2,8 @@ import Quizzes from "@/app/components/Quizzes";
 
 const Home = () => {
   return (
-    <main className="pt-[80px] sm:pl-[190px]">
-      <div className="max-w-screen-xl mx-auto p-4">
+    <main className="pt-[30px] sm:pl-[190px]">
+      <div className="max-w-screen-xl mx-auto px-4">
         <Quizzes />
       </div>
     </main>
